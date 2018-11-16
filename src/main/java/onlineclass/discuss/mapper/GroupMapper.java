379dd13e1462c62fe3a6a1453dac2ac14053d865
Xdeam <wangcilv@gmail.com>
2018-11-16 21:39:30 +0800

    添加通用mybaties组件

---
+++ src/main/java/onlineclass/discuss/mapper/GroupMapper.java
@@ -0,0 +1,8 @@
+package onlineclass.discuss.mapper;
+
+import onlineclass.discuss.model.Group;
+import tk.mybatis.mapper.common.Mapper;
+
+
+public interface GroupMapper extends Mapper<Group> {
+}
---
+++ src/main/java/onlineclass/warning/dao/WarnDao.java
@@ -0,0 +1,35 @@
+package onlineclass.warning.dao;
+
+import core.dao.Dao;
+import onlineclass.warning.mapper.WarnMapper;
+import onlineclass.warning.model.Warn;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class WarnDao implements Dao {
+    @Autowired
+    WarnMapper warnMapper;
+
+    @Override
+    public List selectAll(String sql) {
+        return warnMapper.selectAll();
+    }
+
+    @Override
+    public Object select(Class c, int id) {
+        return warnMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void add(Object object) {
+        warnMapper.insert((Warn) object);
+    }
+
+    @Override
+    public void del(Object x) {
+        warnMapper.delete((Warn) x);
+    }
+}
